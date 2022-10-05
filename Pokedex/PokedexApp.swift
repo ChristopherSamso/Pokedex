f2ddd4ed14e49abfcc802ffce1c4075e845a955c
ChristopherSamso <chsa6996@colorado.edu>
2022-10-05 17:57:59 -0600

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Christopher Samso on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
